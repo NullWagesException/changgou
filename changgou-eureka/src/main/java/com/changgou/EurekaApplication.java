a82786178eed4245fabb3f59342f14be97c4c7de
nullwagesexception <624006385@qq.com>
2019-12-18 17:02:28 +0800

    first commit

---
+++ changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: nullWagesException
+ * @Date: 2019/12/18 16:55
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+
+}
