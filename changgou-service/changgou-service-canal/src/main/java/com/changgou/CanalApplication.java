b07b9a5609fe2486d4fd576aa272dd8da4c994d7
nullwagesexception <624006385@qq.com>
2020-01-09 15:03:18 +0800

    完成canal微服务

---
+++ changgou-service/changgou-service-canal/src/main/java/com/changgou/CanalApplication.java
@@ -0,0 +1,24 @@
+package com.changgou;
+
+import com.xpand.starter.canal.annotation.EnableCanalClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @Author: nullWagesException
+ * @Date: 2020/1/8 12:00
+ * @Description:
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+@EnableCanalClient
+@EnableFeignClients
+public class CanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class,args);
+    }
+}
---
+++ changgou-service/changgou-service-canal/src/main/java/com/changgou/canal/CanalDataEventListener.java
@@ -0,0 +1,70 @@
+package com.changgou.canal;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.changgou.content.feign.ContentFeign;
+import com.changgou.content.pojo.Content;
+import com.xpand.starter.canal.annotation.*;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.List;
+
+/**
+ * @Author: nullWagesException
+ * @Date: 2020/1/8 17:40
+ * @Description:
+ */
+@CanalEventListener
+public class CanalDataEventListener {
+
+    @Autowired
+    private ContentFeign contentFeign;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    /**
+     * @param eventType
+     * @param rowData
+     */
+    @ListenPoint(destination = "example", schema = "changgou_content", table = {"tb_content"}, eventType = {CanalEntry.EventType.UPDATE, CanalEntry.EventType.INSERT, CanalEntry.EventType.DELETE})
+    public void onEventCustomUpdate(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //1.获取到被修改的category_id
+        String categoryId = getColumnValue(eventType, rowData);
+
+        //2.调用feign 获取数据
+        Result<List<Content>> byCategory = contentFeign.findByCategory(Long.valueOf(categoryId));
+        //3.存储到redis中
+        List<Content> data = byCategory.getData();//List
+        //4.
+        stringRedisTemplate.boundValueOps("content_"+categoryId).set(JSON.toJSONString(data));
+    }
+
+    private String getColumnValue(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        //1.判断更改类型 如果是删除 则需要获取到before的数据
+        String categoryId = "";
+        if (CanalEntry.EventType.DELETE == eventType) {
+            List<CanalEntry.Column> beforeColumnsList = rowData.getBeforeColumnsList();
+            for (CanalEntry.Column column : beforeColumnsList) {
+                //column.getName(列的名称   column.getValue() 列对应的值
+                if (column.getName().equals("category_id")) {
+                    categoryId = column.getValue();
+                    return categoryId;
+                }
+            }
+        } else {
+            //2判断是 更新 新增 获取after的数据
+            List<CanalEntry.Column> beforeColumnsList = rowData.getAfterColumnsList();
+            for (CanalEntry.Column column : beforeColumnsList) {
+                //column.getName(列的名称   column.getValue() 列对应的值
+                if (column.getName().equals("category_id")) {
+                    categoryId = column.getValue();
+                    return categoryId;
+                }
+            }
+        }
+        //3.返回
+        return categoryId;
+    }
+}
